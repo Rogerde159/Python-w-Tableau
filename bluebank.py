6f945c6b9bf91367f2aad6178e4245bcae8bec20
Roger <126985040+Rogerde159@users.noreply.github.com>
2024-06-25 17:36:30 -0500

    Update bluebank.py

--- bluebank.py
+++ bluebank.py
@@ -15,30 +15,14 @@ import matplotlib.pyplot as plt
 json_file = open('loan_data_json.json')
 data = json.load(json_file)
 
-# method 2 to read json data
-
-with open('loan_data_json.json') as json_file:
-    data = json.load(json_file)
-
 
 #tranform to dataframe
 loanData = pd.DataFrame(data)
 
-# getting the unique values for the purpose column
-loanData['purpose'].unique()
  
 #describe the data
 # count,mean, std,min, 25%,50%,75%, max
 loanData.describe()
-
-# describe data for a specific column
-loanData['int.rate'].describe()
- 
-# describe on fico scores
-loanData['fico'].describe()
-
-#debt to income ratio
-loanData['dti'].describe()
 
 
 # numpy
@@ -46,61 +30,7 @@ loanData['dti'].describe()
 income = np.exp( loanData['log.annual.inc'])
 loanData['annualIncome'] = income
 
-# working with arrays (1D array)
-arr = np.array([1, 2, 3, 4])
 
-# 0D array
-arr= np.array(43)
-
-# 2D array dimmensional
-
-arr = np.array([[1,2,3], [4,5,6]])
-
-
-# working with if statements
-
-a = 40
-b = 500
-
-if  b > a:
-    print('b is greater than a')
-    
-c = 1000
-
-if b > a and b<c:
-    print('b is greater than a but less than c')
-
-# what if condition isnt met
-
-c = 20
-
-if b > a and b < c:
-    print('b is greater than a but less than c')
-
-else:
-    print('no Conditions met')
-
-# other scenario AND
-c = 30
-
-if b > a and b<c:
-    print('b is greater than a but less than c')
-
-elif b > a and b > c:
-    print('b is greater than a and c')
-    
-else:
-    print('no conditions met')
-
-# other scenario OR
-
-c = 30
-
-if b > a or b<c:
-    print('b is greater than a or less than c')
-  
-else:
-    print('no conditions met')
 
 # fico score
 
@@ -138,20 +68,7 @@ else:
 print(ficocat)
 
 
-# python for looooooops
 
-fruits = ['apple', 'pear', 'banana', 'cherry']
-
-for x in fruits:
-    print(x)
-    y = x + ' fruit'
-    print(y)
-
-for x in range(4):
-    y = fruits[x] + ' for sale'
-    print(y)
-
-# applying for looooops to load data
 
 # using first 10
 
@@ -191,8 +108,6 @@ ficoCat = pd.Series(ficoCat)
 
 loanData['fico.Catergory']= ficoCat
 
-# dataframe.loc as conditional statements
-#df.loc[dataframe[columnName] condition, newColumnName] = 'value if the condition is met'
 
 # for interest rates, a new column is wanted.
 # if rate is > 0.12 high, else low
@@ -203,7 +118,7 @@ loanData.loc[loanData['int.rate'] >0.12, 'int.rate.type' ] = 'High'
 loanData.loc[loanData['int.rate'] <=0.12, 'int.rate.type' ] = 'Low'
 
 
-# matplot lib stuff
+
 # number of loans/rows by fico.category
 
 catPlot = loanData.groupby(['fico.Catergory']).size()
